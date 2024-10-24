6fcb62eec8b6beebdc205e8257e74e06c3018022
Musaddik <musaddikkhan@gmail.com>
2024-10-24 21:33:40 +0530

    Latest Code and Notes updated

---
+++ Arrays Code/src/arraylist.java
@@ -0,0 +1,27 @@
+import javax.sound.midi.Soundbank;
+import java.util.ArrayList;
+import java.util.List;
+
+public class arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        arr.add(2);
+        arr.add(2);
+        arr.add(4);
+        arr.add(5);
+        arr.add(2, 3);
+        arr.remove(3);
+        System.out.println(arr.indexOf(3));
+        arr.addFirst(5);
+//        System.out.println(arr.subList(0, 4));
+//        for(int i = 0;  i < arr.size(); i++){
+//            System.out.print(arr.get(i) + " ");
+//        }
+        for(Integer i : arr){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println(arr);
+    }
+}
