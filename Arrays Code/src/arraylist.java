77d9160abf7a2be841bef6753433c3dd7d92f14c
Musaddik <musaddikkhan@gmail.com>
2024-10-25 21:19:08 +0530

    Code and Notes are pushed

--- Arrays Code/src/arraylist.java
+++ Arrays Code/src/arraylist.java
@@ -4,16 +4,17 @@ import java.util.List;
 
 public class arraylist {
     public static void main(String[] args) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(1);
-        arr.add(2);
-        arr.add(2);
-        arr.add(4);
-        arr.add(5);
-        arr.add(2, 3);
-        arr.remove(3);
-        System.out.println(arr.indexOf(3));
-        arr.addFirst(5);
+        ArrayList<Integer> arr = new ArrayList<Integer>(10);
+//        arr.add(1);
+//        arr.add(2);
+//        arr.add(2);
+//        arr.add(4);
+//        arr.add(5);
+//        arr.add(2, 3);
+//        arr.remove(3);
+//        System.out.println(arr.indexOf(3));
+//        arr.addFirst(5);
+
 //        System.out.println(arr.subList(0, 4));
 //        for(int i = 0;  i < arr.size(); i++){
 //            System.out.print(arr.get(i) + " ");
